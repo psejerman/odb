959efef16d6a89f7ce1cc07aab602ca52909607c
Peter Sejerman <p.sejerman@gmail.com>
2021-05-31 16:16:29 +0200

    Package.html Tests

--- src/main/java/de/project/odb/Customer.java
+++ src/main/java/de/project/odb/Customer.java
@@ -1,5 +1,11 @@
 package de.project.odb;
 
+/**
+ * <h1>Customer</h1>
+ * <h3>Testklasse</h3>
+ * <p>Beispiel-Klasse(Ableitung 2.Grades) zum Testen der Grundfunktionen von ODB. Kann durch jede andere klasse ersetzt werden
+ * Besitzt Basisattribute und Standardsetter und Standardgetter</p>
+ */
 public class Customer extends Person{
 
     private int customerID;
